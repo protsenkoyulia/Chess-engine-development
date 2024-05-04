ecac1e2d48a544e008e51ab066d3bcbf4f88d61a
ccherkess <misha.tcherkasov2015@gmail.com>
2024-05-04 04:47:54 +0300

    remove unused files

--- cmake-build-debug/CMakeFiles/chess_engine_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/chess_engine_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/chess_engine_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
